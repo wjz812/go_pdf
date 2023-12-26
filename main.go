ec8014f45e02c2276770677d1b31160631a6fa71
jz.wang <jz.wang@startiasoft.com>
2023-12-26 08:48:24 +0800

    提交cad to pdf 测试

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "gopdf/pdf"
+import (
+	"gopdf/cad"
+)
 
 func main() {
 	//pdf.BaseText()
@@ -21,5 +23,15 @@ func main() {
 	// pdf.ReadPdf7()
 	// pdf.ReadPdf8()
 	// pdf.ReadPdf9()
-	pdf.ReadPdf10()
+	// pdf.ReadPdf10()
+
+	cad.CadToPdf()
+	// cad.Read01()
+	// cad.Read02()
+	// cad.Write01()
+	// cad.Read03()
+	// cad.Read04()
+	// cad.ReadDxf()
+	// cad.Read05()
+	// cad.Read()
 }
